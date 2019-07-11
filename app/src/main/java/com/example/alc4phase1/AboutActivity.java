ed98d191bde84c647c8b2a2c94d2dc34e5d53e35
Alhussiny <eng.elhoseny.osama@gmail.com>
2019-07-11 23:45:23 +0200

    intially done

--- app/src/main/java/com/example/alc4phase1/AboutActivity.java
+++ app/src/main/java/com/example/alc4phase1/AboutActivity.java
@@ -1,6 +1,8 @@
 package com.example.alc4phase1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.net.http.SslError;
 import android.os.Bundle;
@@ -19,6 +21,20 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
+
+        // my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar =
+                (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+
         WebView webView = findViewById(R.id.webview);
 //        URI uri = URI.create("https://andela.com/alc");
         webView.getSettings().setJavaScriptEnabled(true);
--- app/src/main/java/com/example/alc4phase1/ProfileActivity.java
+++ app/src/main/java/com/example/alc4phase1/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.alc4phase1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,18 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
+
+        // my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar =
+                (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
