215655a133fb0131e477de8d9ac63a418244ff58
Alhussiny <eng.elhoseny.osama@gmail.com>
2019-07-11 22:25:04 +0200

    finish mainactivity polish

--- app/src/main/java/com/example/alc4phase1/MainActivity.java
+++ app/src/main/java/com/example/alc4phase1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.alc4phase1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.graphics.Typeface;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Note that the Toolbar defined in the layout has the id "my_toolbar"
+        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
 
         Button buttonAbout = findViewById(R.id.btn_about);
         buttonAbout.setOnClickListener(new View.OnClickListener() {
